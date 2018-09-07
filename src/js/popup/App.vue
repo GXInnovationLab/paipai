30c4ff29b2c46bbd7c3b0f2667ed1c9faf794605
MarquisF <marquisfffff@gmail.com>
2018-09-07 15:35:32 +0800

    Update App.vue

---
+++ src/js/popup/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import backgroundProtocol from 'BackgroundProtocol';
+import api from 'Popup/backgroundApi';
+import router from './router';
+
+export default {
+  name: 'App',
+  mounted: async function() {
+    // const msg = {
+    //   command: backgroundProtocol.GET_STATUS_URL
+    // }
+
+    const route = await api.command(backgroundProtocol.GET_STATUS_URL);
+    console.log('route: ', route)
+    router.push(route);
+  }
+}
+
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  width: 315px;
+  height: 400px;
+}
+</style>

a88a77f9b42867992a24ff9da6669e14180dbc86
MarquisF <marquisfffff@gmail.com>
2018-09-07 15:35:18 +0800

    Update index.js

---
+++ src/js/popup/router/index.js
@@ -0,0 +1,35 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import Home from 'Components/Home';
+import NewStory from 'Components/NewStory';
+import Display from 'Components/Display';
+Vue.use(Router);
+
+export const paths = {
+  newStory: '/new'
+}
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/home',
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: paths.newStory,
+      name: 'New Story',
+      component: NewStory
+    },
+    {
+      path: '/display/:id',
+      name: 'Display',
+      component: Display
+    }
+  ]
+});
